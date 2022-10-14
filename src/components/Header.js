c09c233f2d81f68cf193ffe37507d3ad9145b0d9
Ayushi Rai <ayushicodes@gmail.com>
2022-10-14 22:15:36 +0530

    Header created

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+        <div className="header_main">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaqHIu0speA5qckni84Jw1M4uHNmSLBuoFUw&usqp=CAU" alt="logo" className="header" />
+            <h1 className="heading">Meme Generator</h1>
+        </div>
+    )
+}
+export default Header
