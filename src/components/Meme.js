dbeb51824a1fdae33e8fd8ee54c29038389364b4
Ayushi Rai <ayushicodes@gmail.com>
2023-04-05 01:22:34 +0530

    data fetched

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,18 +1,28 @@
-import React, { useState } from "react"
-import memedata from "./memedata"
+import React, { useState, useEffect } from "react"
+// import memedata from "./memedata"
 
 function Meme() {
-  const [image, setImage] = useState("")
+  const [image, setImage] = useState([])
   const [formData, setFormData] = useState({
     topText: "",
     bottomText: "",
+    randomImage: "https://i.imgflip.com/1e7ql7.jpg",
+  })
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setImage(data.data.memes))
   })
 
   function getImage(e) {
     e.preventDefault()
-    let data = memedata.data.memes
-    const randomNumber = Math.floor(Math.random() * data.length)
-    setImage(data[randomNumber].url)
+    // let data = image.data.memes
+    const randomNumber = Math.floor(Math.random() * image.length)
+    console.log("randomnumber", randomNumber)
+    let url = image[randomNumber].url
+    console.log("url", url)
+    setFormData((prevFormData) => ({ ...prevFormData, randomImage: url }))
   }
 
   function handleChange(e) {
@@ -43,7 +53,7 @@ function Meme() {
           Get The Images
         </button>
         <div>
-          <img className="img-display" src={image} alt="" />
+          <img className="img-display" src={formData.randomImage} alt="" />
           <h1>{formData.topText}</h1>
           <h1>{formData.bottomText}</h1>
         </div>
