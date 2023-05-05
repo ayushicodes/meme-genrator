2a6cb9ea8722e5e41110f6e86701a3192d13d188
Ayushi Rai <ayushicodes@gmail.com>
2023-05-05 14:53:21 +0530

    headingtitle positon updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
-import React from "react"
-import "./Meme.css"
+import React from "react";
+import "./Meme.css";
 
 function Header() {
   return (
     <div className="header">
-      <img
+      {/* <img
         className="header--image"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScImjmkngwkjoGBp58QyJmC7SNlI8pKT-7bQ&usqp=CAU
         "
         alt="logo"
-      />
+      />*/}
       <h1 className="header--title">Meme Generator</h1>
     </div>
-  )
+  );
 }
-export default Header
+export default Header;
